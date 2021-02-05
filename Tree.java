ac8670881d204cbe1aed5f1cea701a9cefbd1330
timothymichaelcook <75735609+timothymichaelcook@users.noreply.github.com>
2021-02-05 13:58:55 -0500

    Add files via upload

---
+++ Tree.java
@@ -0,0 +1,12 @@
+public class Tree {
+  public static void main(String[] args) {
+    //This code will introduce ourselves and plant a tree to the screen
+    System.out.println("Hey there, I'm Tim and I'm learning to code in Java!");
+    System.out.println("I'm going to plant a tree today on the screen!");
+    System.out.println("  *  ");
+    System.out.println(" *** ");
+    System.out.println("*****");
+    System.out.println("  *  ");
+
+  }
+}
